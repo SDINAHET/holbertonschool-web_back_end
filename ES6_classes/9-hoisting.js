c3ccbed3818cd8c2d989e9111869910f24340f4b
SDINAHET <stephane.dinahet@gmail.com>
2024-11-12 20:30:30 +0100

    Refactor 9-hoisting.js to streamline student creation; replace individual instances with a mapped array for improved readability

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -38,10 +38,22 @@ export class StudentHolberton {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+// const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+// const student2 = new StudentHolberton('John', 'Doe', class2020);
+// const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+// const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+// const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+// export const listOfStudents = [student1, student2, student3, student4, student5];
+
+// Création des étudiants
+const students = [
+  ['Guillaume', 'Salva', class2020],
+  ['John', 'Doe', class2020],
+  ['Albert', 'Clinton', class2019],
+  ['Donald', 'Bush', class2019],
+  ['Jason', 'Sandler', class2019],
+].map(([firstName, lastName, holbertonClass]) => new StudentHolberton(firstName, lastName,
+  holbertonClass));
+
+export const listOfStudents = students;
