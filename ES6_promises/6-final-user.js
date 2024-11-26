8e9252536a59869a7e0690b541d636dabd62a497
SDINAHET <stephane.dinahet@gmail.com>
2024-11-26 17:07:44 +0100

    Add initial implementations for ES6 promises and data manipulation functions

---
+++ ES6_promises/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo]).then((results) => {
+    return results.map((result) => ({
+      status: result.status,
+      value: result.value || result.reason,
+    }));
+  });
+}
