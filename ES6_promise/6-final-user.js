ccfa6e6b5daf2082e2e65b7be2e8ac49ddc3ca53
SDINAHET <stephane.dinahet@gmail.com>
2024-11-28 03:01:14 +0100

    Refactor handleProfileSignup function: convert promise handling to async/await for improved readability and maintainability; update import statements to include file extensions

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,31 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }))
-    ));
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
+  }));
 }
+
+// import signUpUser from './4-user-promise';
+// import uploadPhoto from './5-photo-reject';
+
+// export default async function handleProfileSignup(firstName, lastName, fileName) {
+//   return Promise
+//     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+//     .then((res) => (
+//       res.map((o) => ({
+//         status: o.status,
+//         value: o.status === 'fulfilled' ? o.value : String(o.reason),
+//       }))
+//     ));
+// }
 
 // // 6-final-user.js
 // import signUpUser from './4-user-promise.js';
--- ES6_promise/README.md
+++ ES6_promise/README.md
@@ -3,3 +3,5 @@
 ![Project badge](https://img.shields.io/badge/Progress-0%25-red)
 - **Your score will be updated as you progress.**
 
+npx eslint 6-final-user.js
+npm run full-test
