c713672f61db5cc87b54b1438d9aa603d539a4e3
SDINAHET <stephane.dinahet@gmail.com>
2024-11-28 11:09:59 +0100

    Refactor handleProfileSignup function: convert to async/await for improved readability; streamline promise handling and enhance code organization

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,34 +1,33 @@
-// 6-final-user.js
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
-
-export default function handleProfileSignup(firstName, lastName, fileName) {
-    // Appeler les fonctions et gérer les promesses
-    const signUpPromise = signUpUser(firstName, lastName);
-    const uploadPromise = uploadPhoto(fileName);
-
-    return Promise.allSettled([signUpPromise, uploadPromise])
-        .then(results => results.map(result => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason
-        })));
-}
-
-
+// // 6-final-user.js
 // import signUpUser from './4-user-promise.js';
 // import uploadPhoto from './5-photo-reject.js';
 
-// export default async function handleProfileSignup(firstName, lastName, fileName) {
-//   const results = await Promise.allSettled([
-//     signUpUser(firstName, lastName),
-//     uploadPhoto(fileName),
-//   ]);
+// export default function handleProfileSignup(firstName, lastName, fileName) {
+//     // Appeler les fonctions et gérer les promesses
+//     const signUpPromise = signUpUser(firstName, lastName);
+//     const uploadPromise = uploadPhoto(fileName);
 
-//   return results.map((result) => ({
-//     status: result.status,
-//     value: result.status === 'fulfilled' ? result.value : String(result.reason),
-//   }));
+//     return Promise.allSettled([signUpPromise, uploadPromise])
+//         .then(results => results.map(result => ({
+//             status: result.status,
+//             value: result.status === 'fulfilled' ? result.value : result.reason
+//         })));
 // }
+
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
+  }));
+}
 
 // import signUpUser from './4-user-promise';
 // import uploadPhoto from './5-photo-reject';
