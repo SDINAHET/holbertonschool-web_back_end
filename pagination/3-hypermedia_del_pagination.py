b80b1dafb190491d7dc26098e891479819a02306
SDINAHET <stephane.dinahet@gmail.com>
2024-12-10 00:19:17 +0100

    Implement deletion-resilient hypermedia pagination with dataset caching

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,14 +1,45 @@
 #!/usr/bin/env python3
 """
-Server class with deletion-resilient hypermedia pagination.
+Deletion-resilient hypermedia pagination
 """
-from typing import Dict
+
+import csv
+import math
+from typing import List, Dict
 # from 2-hypermedia_pagination import Server
 Server = __import__('2-hypermedia_pagination').Server
 
 
-class Server(Server):
-    """Server class with deletion-resilient hypermedia pagination."""
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
