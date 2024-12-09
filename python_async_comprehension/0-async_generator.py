ca2db1d4d6f7e33288e45f479d86b7196e723437
SDINAHET <stephane.dinahet@gmail.com>
2024-12-09 12:56:12 +0100

    Uncomment async_generator function definition for proper functionality

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,7 +9,8 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+# async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator():
     """
     Génère 10 nombres aléatoires entre 0 et 10, avec une pause de 1 seconde
     entre chaque.
