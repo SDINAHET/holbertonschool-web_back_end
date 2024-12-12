bbeb289c66975c66463e14a69891a3cb347b0641
SDINAHET <stephane.dinahet@gmail.com>
2024-12-12 23:37:05 +0100

    Refactor update_topics function to improve docstring and ensure functionality

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -9,18 +9,20 @@ in a MongoDB collection based on the school name.
 from pymongo.collection import Collection
 
 def update_topics(mongo_collection: Collection, name: str, topics: list):
-    """
-    Updates the topics of a school document in the collection based on the name.
+    # """
+    # Updates the topics of a school document in the collection based on the name.
 
-    Args:
-        mongo_collection (Collection): pymongo collection object
-        name (str): The school name to update
-        topics (list): The list of topics to set for the school
+    # Args:
+    #     mongo_collection (Collection): pymongo collection object
+    #     name (str): The school name to update
+    #     topics (list): The list of topics to set for the school
 
-    Returns:
-        None
-    """
-    mongo_collection.update_many(
-        {"name": name},
-        {"$set": {"topics": topics}}
-    )
+    # Returns:
+    #     None
+    # """
+    # mongo_collection.update_many(
+    #     {"name": name},
+    #     {"$set": {"topics": topics}}
+    # )
+    """changes all topics of a school document based on the name"""
+    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
