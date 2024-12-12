9d5f01ab81903e7df1b2319a442697ba8b189703
SDINAHET <stephane.dinahet@gmail.com>
2024-12-12 23:43:57 +0100

    Add schools_by_topic function to retrieve schools by topic from MongoDB

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module to find schools by topic in a MongoDB collection.
+
+This script defines a function to search for schools that have a specific topic
+in their "topics" field.
+"""
+
+from pymongo.collection import Collection
+
+def schools_by_topic(mongo_collection: Collection, topic: str):
+    """
+    Returns the list of schools having a specific topic.
+
+    Args:
+        mongo_collection (Collection): pymongo collection object
+        topic (str): Topic to search for
+
+    Returns:
+        List[dict]: List of documents (schools) that contain the topic
+    """
+    return list(mongo_collection.find({"topics": topic}))
