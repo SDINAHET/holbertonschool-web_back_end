1afab0d84248a17cf992f9e072925daa123db9f4
SDINAHET <stephane.dinahet@gmail.com>
2024-12-12 20:01:12 +0100

    Add insert_school function to insert documents into MongoDB collection

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module to insert a new document in a MongoDB collection.
+
+This script defines a function to insert a document into a MongoDB collection
+using the pymongo library. It accepts key-value pairs as input and returns the
+_id of the newly inserted document.
+"""
+
+from pymongo.collection import Collection
+
+def insert_school(mongo_collection: Collection, **kwargs):
+    """
+    Inserts a new document in a MongoDB collection based on kwargs.
+
+    Args:
+        mongo_collection (Collection): pymongo collection object
+        **kwargs: Key-value pairs representing the document fields
+
+    Returns:
+        ObjectId: The new document's _id.
+    """
+    new_document = kwargs
+    result = mongo_collection.insert_one(new_document)
+    return result.inserted_id
