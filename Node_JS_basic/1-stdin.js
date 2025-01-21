2f2bc0fdf3b9c26dac7b2683a4274367c15767fa
SDINAHET <stephane.dinahet@gmail.com>
2025-01-22 00:51:13 +0100

    Refactor stdin handling to use consistent variable naming for improved readability

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,19 @@
 // 1-stdin.js
 const readline = require('readline');
 
-const rl = readline.createInterface({
+const ui = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.question('', (name) => {
+ui.question('', (name) => {
   console.log(`Your name is: ${name}`);
-  rl.close();
+  ui.close();
 });
 
-rl.on('close', () => {
+ui.on('close', () => {
   console.log('This important software is now closing');
   process.exit(0); // Explicitly exit the process
 });
