317a824e48daf2d45fe9b6aefafa4bba21722c44
SDINAHET <stephane.dinahet@gmail.com>
2025-07-23 19:34:13 +0200

    Add close_db_connection function to safely close MySQL database connections

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -120,7 +120,15 @@ def main():
 
     cursor.close()
     # Closing the database connection (native MySQLConnection method)
+def close_db_connection(db: MySQLConnection) -> None:
+    """
+    Closes the database connection safely.
+
+    Args:
+        db: MySQLConnection object.
+    """
     db.close()
+
 
 
 if __name__ == "__main__":
