d254d773ec2379339b92a3ca2da686e5840be01c
SDINAHET <stephane.dinahet@gmail.com>
2025-07-23 13:26:54 +0200

    Refactor filter_datum function for improved readability and maintainability

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,5 +7,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    return re.sub(rf'({"|".join(fields)})=.*?{separator}', lambda m: f"{m.group(1)}={redaction}{separator}", message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    pattern = rf'({"|".join(fields)})=.*?{separator}'
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{separator}",
+                  message)
