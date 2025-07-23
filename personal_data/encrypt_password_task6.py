91517f3341f9d8e70795d4637672b350a5483a29
SDINAHET <stephane.dinahet@gmail.com>
2025-07-23 20:15:35 +0200

    Remove unnecessary blank line in password hashing module

--- personal_data/encrypt_password_task6.py
+++ personal_data/encrypt_password_task6.py
@@ -18,6 +18,7 @@ def hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     Validates that the provided password matches the hashed password.
