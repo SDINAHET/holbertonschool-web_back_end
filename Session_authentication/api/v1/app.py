06e59403bd2652e4343938140906894ccf6fa445
SDINAHET <stephane.dinahet@gmail.com>
2025-07-31 04:09:33 +0200

    task5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -90,9 +90,9 @@ def before_request_func():
 
     # if auth.authorization_header(request) is None:
     #     abort(401)
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
-
 
     user = auth.current_user(request)  # ✅ Tu avais oublié cette ligne
     if user is None:
