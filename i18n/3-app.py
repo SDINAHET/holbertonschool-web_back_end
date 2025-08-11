6971f5bd791472523af157aebc9cf14fda2ab1ea
SDINAHET <stephane.dinahet@gmail.com>
2025-08-11 23:07:36 +0200

    task3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -5,6 +5,7 @@ Flask app: parametrized templates with Flask-Babel
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 class Config:
     """
     Application configuration for Flask-Babel.
@@ -18,10 +19,12 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel()
+
 
 def get_locale():
     """
@@ -32,7 +35,9 @@ def get_locale():
     """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
+
 babel.init_app(app, locale_selector=get_locale)
+
 
 @app.route("/")
 def index():
@@ -43,6 +48,7 @@ def index():
         str: Rendered HTML of templates/3-index.html.
     """
     return render_template("3-index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
